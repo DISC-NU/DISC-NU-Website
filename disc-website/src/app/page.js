1c7faeada29e70d3bb8c074e066632e9e6e365f3
PaulaFregene <paulafregene@gmail.com>
2024-02-28 20:20:19 -0600

    update page.js
    removed the default code to have a plaveholder code

--- disc-website/src/app/page.js
+++ disc-website/src/app/page.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PastProjects from './components/past-projects.js';
 
+
+
 export default function Home() {
-  return PastProjects; //todo: adjust this and the import once get template code fixed in past-projects.js
+  return <div> About</div>
 }
