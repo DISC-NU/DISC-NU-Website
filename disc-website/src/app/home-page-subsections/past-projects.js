92672af4b3a22ca5b72694211d7ba8a8b0f8d710
Joanna Soltys <jmsoltys26@gmail.com>
2024-04-08 20:17:09 -0500

    added gaps between images in past projects photo gallery

--- disc-website/src/app/home-page-subsections/past-projects.js
+++ disc-website/src/app/home-page-subsections/past-projects.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-//todo: try to find an image grid in tailwind instead cuz material UI requires installs (try out shad??)
-
 
 function PastProjects() {
   return (
@@ -8,25 +6,23 @@ function PastProjects() {
       {/* ----------- SECTION HEADER --------------- */}
       {/* //todo: confirm CSS that we want too apply to all section headers */}
       {/* //todo: ask Paula about if she's pushed the fonts */}
-      {/* //todo: also confirm  */}
       <h2 className="home-page-subsection-title">Past Projects</h2>
 
       {/* ----------- START OF IMAGE GALLERY ------------ */}
       {/* //todo: look into project photos that we can upload here */}
-      {/* //todo: ask Amy for guidance on making the gallery responsive */}
       <div className="flex flex-row" id="past-projects-gallery">
         
         {/* Vertical Image (like an app) */}
-        <img src="https://placehold.co/60x120"></img>
+        <img src="https://placehold.co/60x135" id="vertical-proj-img"></img>
 
         {/* Horizontal Image (like a website) */}
-        <div className = "flex flex-col">
+        <div className = "flex flex-col" id="big-horizontal-proj-img">
           <img src="https://placehold.co/60x40"></img>
 
           {/* Two Smaller Horizontal Images */}
           <div className="flex flex-row">
-            <img src="https://placehold.co/60x40"></img>
-            <img src="https://placehold.co/60x40"></img>
+            <img src="https://placehold.co/60x40" id="small-left-horizontal-proj-img"></img>
+            <img src="https://placehold.co/60x40" id="small-right-horizontal-proj-img"></img>
           </div>
         </div>
       </div>
