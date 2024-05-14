d0c316d11d90de923f74b78917e9e2b6c357467f
Wakiuru Wachiuri <109316261+WakiuruP@users.noreply.github.com>
2024-05-13 20:08:20 -0500

    Created the welcome/landing page
    This is the welcome page for the entire website from where users can navigate to different parts. Still needs a little polishing but most of the work is done.

---
+++ disc-website/src/app/components/welcome.js
@@ -0,0 +1,68 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import Typography from '@mui/material/Typography';
+import './welcome.css';
+
+function Welcome() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen((window.innerHeight < 600 || window.innerWidth < 600)); // Adjust the threshold as needed
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div className="welcome-page">
+      {/* Content for the left column */}
+      <div className="left-column" style={{ justifyContent: "center" }}>
+        <Typography variant="h2" noWrap className="welcome-message-p1">
+          WELCOME TO
+        </Typography>
+        {!isSmallScreen && (
+          <Typography variant="h2" noWrap className="welcome-message-p2">
+            DISC NU
+          </Typography>
+        )}
+        <Typography variant="h3" className={`${isSmallScreen ? 'welcome-message-p2 small-screen-p2' : 'welcome-message-p3'}`}>
+          {!isSmallScreen ? (
+            <>
+              A Northwestern University <br /> community dedicated to <br /> Designing & Innovating for <br />Social Change
+              <div className='flex space-x-4' style={{ justifyContent: "center", padding: '20px' }}>
+                <a href="https://www.facebook.com/discNU/">
+                  <img class= 'icon-gallery' src= 'Facebook.png'/>
+                </a>
+                <a href="https://www.instagram.com/disc_nu/">
+                  <img class= 'icon-gallery' src= 'Instagram.png'/>
+                </a>
+                <a href="https://www.linkedin.com/company/northwestern-develop-innovation-for-social-change-disc/">
+                  <img class= 'icon-gallery' src= 'LinkedIn.png'/>
+                </a>
+                <a href="https://github.com/DISC-NU" >
+                  <img class= 'icon-gallery' src= 'Github.png'/>
+                </a>
+                <a href="mailto:disc@u.northwestern.edu">
+                  <img class= 'icon-gallery' src='Email.png'/>
+                </a>
+              </div>
+            </>
+          ) : (
+            "DISC NU"
+          )}
+        </Typography>
+      </div>
+
+      {/* Content for the right column */}
+      <div className="right-column" style={{ justifyContent: "center" }}>
+        <img src="disc logo.png" className={`welcome-logo ${isSmallScreen ? 'small-logo' : ''}`} alt="Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
--- disc-website/src/app/page.js
+++ disc-website/src/app/page.js
@@ -1,4 +1,5 @@
 import ResponsiveNavBar from './components/navbar.js'
+import Welcome from './components/welcome.js'
 import PastProjects from './components/past-projects.js';
 import Footer from './components/footer'; 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div>
       <ResponsiveNavBar />
+      <Welcome />
       <PastProjects />
       <Footer />
     </div>
