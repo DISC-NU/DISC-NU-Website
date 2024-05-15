598c0772ed5925545d54a67b99a2da5a804f53db
Joanna Soltys <jmsoltys26@gmail.com>
2024-05-15 14:46:03 -0500

    Fixed "class" to "className" in footer.js

--- disc-website/src/app/components/footer.js
+++ disc-website/src/app/components/footer.js
@@ -2,7 +2,7 @@ export default function Footer () {
     return ( 
         <div className = "footer" style= {{display: "grid", gridTemplateColumns: "3fr 1fr"}}>
             <div>
-                <div class="flex justify-start">
+                <div className="flex justify-start">
                     <div>
                         <p className = 'footerText'>Copyright © DISC NU </p>
                         <p className = 'footerText'>Website Designed and Developed In-House: </p>
@@ -13,19 +13,19 @@ export default function Footer () {
 
             <div class='flex space-x-4' style={{justifyContent: "center"}}>
                     <a href="https://www.facebook.com/discNU/">
-                        <img class= 'icon-gallery' src= 'Facebook.png'/>
+                        <img className= 'icon-gallery' src= 'Facebook.png'/>
                     </a>
                     <a href="https://www.instagram.com/disc_nu/">
-                        <img class= 'icon-gallery' src= 'Instagram.png'/>
+                        <img className= 'icon-gallery' src= 'Instagram.png'/>
                     </a>
                     <a href="https://www.linkedin.com/company/northwestern-develop-innovation-for-social-change-disc/">
-                        <img class= 'icon-gallery' src= 'LinkedIn.png'/>
+                        <img className= 'icon-gallery' src= 'LinkedIn.png'/>
                     </a>
                     <a href="https://github.com/DISC-NU" >
-                        <img class= 'icon-gallery' src= 'Github.png'/>
+                        <img className= 'icon-gallery' src= 'Github.png'/>
                     </a>
                     <a href="mailto:disc@u.northwestern.edu">
-                        <img class= 'icon-gallery' src='Email.png'/>
+                        <img className= 'icon-gallery' src='Email.png'/>
                     </a>
 
                 </div>
